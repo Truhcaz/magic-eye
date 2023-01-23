f10d1309483d62d4e2c2e26834edef005ee01d91
Truhcaz <mateo.brc1@gmail.com>
2023-01-23 19:37:53 +0100

    api url update

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -20,7 +20,7 @@ class SignIn extends Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://magic-eye-api.onrender.com/signin', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
